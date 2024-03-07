0b036abe46040c57085754f509f5b652df0aacc2
jbla20 <72883493+jbla20@users.noreply.github.com>
2024-03-07 13:46:39 +0100

    Made fingers open and close

--- hand_prosthesis_rl/src/training/train.py
+++ hand_prosthesis_rl/src/training/train.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
+import numpy as np
 from robot_envs.mia_hand_env import MiaHandEnv
 
 
@@ -8,10 +9,19 @@ def main():
     mia_env = MiaHandEnv()
     
     rospy.sleep(5)
-    #mia_env.move_finger(speed=0.5, finger_id="thumb")
-    mia_env.move_fingers(speeds=[0.5, 0.5, 0.5])
     
-    rospy.spin()
+    while not rospy.is_shutdown():
+        current_time = rospy.get_time()
+        
+        # Alternating between opening and closing hand
+        alternating_time = 5
+        if (int(current_time) % (2*alternating_time)) < alternating_time:
+            speed = 0.5
+        else:
+            speed = -0.5
+        mia_env.move_fingers(np.repeat(speed, 3))
+        
+        rospy.sleep(0.1)
 
 if __name__ == "__main__":
     rospy.init_node("mia_hand_rl_env")
