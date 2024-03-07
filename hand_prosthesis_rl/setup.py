aa2554abfaf5d10dcd52c278afe0c9a341fdbe24
jbla20 <72883493+jbla20@users.noreply.github.com>
2024-03-07 12:19:58 +0100

    Changed folder structure and added script that initialises the mia hand env

--- hand_prosthesis_rl/setup.py
+++ hand_prosthesis_rl/setup.py
@@ -5,7 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['hand_prosthesis_rl'],
+    script=['training/train.py'],
+    packages=['gazebo', 'robot_envs', 'training'],
     package_dir={'': 'src'},
 )
 
