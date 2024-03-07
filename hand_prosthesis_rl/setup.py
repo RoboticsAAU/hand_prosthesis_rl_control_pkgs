bb3332d3f59d1bc854392b90e0e68d785fae078e
DaniDoro <daniel.d@outlook.dk>
2024-03-07 13:07:10 +0000

    Began writing task_env for mia hand and wrote/loaded mia_hand param file

--- hand_prosthesis_rl/setup.py
+++ hand_prosthesis_rl/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     script=['training/train.py'],
-    packages=['gazebo', 'robot_envs', 'training'],
+    packages=['gazebo', 'robot_envs', 'task_env', 'training'],
     package_dir={'': 'src'},
 )
 
