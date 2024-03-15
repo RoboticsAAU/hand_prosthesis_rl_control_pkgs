aa05ec23adbd58971acc8dffe9764c56c0cee86c
DaniDoro <daniel.d@outlook.dk>
2024-03-15 11:54:43 +0100

    Cleaned up and added functions for action and observation space (not done)

--- hand_prosthesis_rl/src/hand_prosthesis_rl/task_envs/mia_hand_task_env.py
+++ hand_prosthesis_rl/src/hand_prosthesis_rl/task_envs/mia_hand_task_env.py
@@ -1,9 +1,10 @@
 import rospy
-import numpy
+import numpy as np
 from gym import spaces
 from hand_prosthesis_rl.robot_envs import mia_hand_env
 from gym.envs.registration import register
 from geometry_msgs.msg import Vector3
+from functools import cached_property
 
 # The path is __init__.py of openai_ros, where we import the TurtleBot2MazeEnv directly
 timestep_limit_per_episode = 10000 # Can be any Value
@@ -29,31 +30,17 @@ class MiaHandWorldEnv(mia_hand_env.MiaHandEnv):
         self.mrl_vel_lb = rospy.get_param('/mia_hand/mrl_vel_lb')
         self.mrl_vel_ub = rospy.get_param('/mia_hand/mrl_vel_ub')
         
-        as_low = numpy.array([self.index_vel_lb,
-                            self.thumb_vel_lb,
-                            self.mrl_vel_lb])
-        
-        as_high = numpy.array([self.index_vel_ub, 
-                            self.thumb_vel_ub,
-                            self.mrl_vel_ub])
-        
-        self.action_space = spaces.Box(as_low, as_high, dtype = numpy.float32)
+        # Bounds for joint velocities
+        self.vel_lb = np.array([self.index_vel_lb, self.thumb_vel_lb, self.mrl_vel_lb])
+        self.vel_ub = np.array([self.index_vel_ub, self.thumb_vel_ub, self.mrl_vel_ub])
         
         # We set the reward range, which is not compulsory but here we do it.
-        self.reward_range = (-numpy.inf, numpy.inf)
+        self.reward_range = (-np.inf, np.inf)
                 
         # Initial velocities
         self.init_index_vel = 0
         self.init_thumb_vel = 0
         self.init_mrl_vel = 0
-        
-        
-        # We create two arrays based on the binary values that will be assigned
-        # In the discretization method.
-        # laser_scan = self._check_laser_scan_ready()
-        # num_laser_readings = len(laser_scan.ranges)/self.new_ranges
-        # high = numpy.full((num_laser_readings), self.max_laser_value)
-        # low = numpy.full((num_laser_readings), self.min_laser_value)
         
         # Define the upper and lower bounds for positions
         self.index_pos_lb = rospy.get_param('/mia_hand/index_pos_lb')
@@ -63,24 +50,31 @@ class MiaHandWorldEnv(mia_hand_env.MiaHandEnv):
         self.mrl_pos_lb = rospy.get_param('/mia_hand/mrl_pos_lb')
         self.mrl_pos_ub = rospy.get_param('/mia_hand/mrl_pos_ub')
         
-        os_low = numpy.array([self.index_pos_lb,
-                            self.thumb_pos_lb,
-                            self.mrl_pos_lb])
+        # Bounds for joint positions        
+        self.pos_lb = np.array([self.index_pos_lb, self.thumb_pos_lb, self.mrl_pos_lb])
+        self.pos_ub = np.array([self.index_pos_ub, self.thumb_pos_ub, self.mrl_pos_ub])
         
-        os_high = numpy.array([self.index_pos_ub, 
-                            self.thumb_pos_ub,
-                            self.mrl_pos_ub])
-        
-        # We only use two integers
-        self.observation_space = spaces.Box(os_low, os_high, dtype = numpy.float32)
-        
-        rospy.logdebug("ACTION SPACES TYPE===>"+str(self.action_space))
-        rospy.logdebug("OBSERVATION SPACES TYPE===>"+str(self.observation_space))
+        rospy.logdebug("ACTION SPACES TYPE===>"+str(self._action_space()))
+        rospy.logdebug("OBSERVATION SPACES TYPE===>"+str(self._obs_space()))
         
         # Rewards
         self.end_episode_points = rospy.get_param("/mia_hand/end_episode_points")
 
         self.cumulated_steps = 0.0
+
+        self.camera_infos = {
+            "wrist_cam": {
+                "point_cloud": {
+                    "num_points": 512,
+                    "fov_x": 60,
+                    "fov_y": 60,
+                    "max_range": 1.5,
+                    "min_range": 0.5
+                }
+            }
+        }
+        
+        self.use_imagined = True
 
         # Here we will add any init functions prior to starting the MyRobotEnv
         super(MiaHandWorldEnv, self).__init__()
@@ -89,7 +83,7 @@ class MiaHandWorldEnv(mia_hand_env.MiaHandEnv):
         """Sets the Robot in its init pose
         """
         
-        velocities = numpy.array([self.init_index_vel,
+        velocities = np.array([self.init_index_vel,
                                 self.init_thumb_vel,
                                 self.init_mrl_vel])
         
@@ -131,7 +125,7 @@ class MiaHandWorldEnv(mia_hand_env.MiaHandEnv):
         #index_vel, thumb_vel, mrl_vel = action[0], action[1], action[2]
         
         # Mia hand is set to execute the speeds
-        velocities = numpy.array([index_vel, thumb_vel, mrl_vel]) 
+        velocities = np.array([index_vel, thumb_vel, mrl_vel]) 
         
         self.move_fingers(velocities)
         
@@ -154,7 +148,7 @@ class MiaHandWorldEnv(mia_hand_env.MiaHandEnv):
         rospy.logdebug("Observations==>"+str(discretized_observations))
         rospy.logdebug("END Get Observation ==>")
         return discretized_observations
-        
+
 
     def _is_done(self, observations):
         
@@ -174,6 +168,7 @@ class MiaHandWorldEnv(mia_hand_env.MiaHandEnv):
         
 
         return self._episode_done
+
 
     def _compute_reward(self, observations, done):
 
@@ -195,49 +190,37 @@ class MiaHandWorldEnv(mia_hand_env.MiaHandEnv):
         return reward
 
 
-    # Internal TaskEnv Methods
+    @cached_property
+    def _action_space(self):
+        return spaces.Box(self.vel_lb, self.vel_ub, dtype = np.float32)
+
+
+    @cached_property
+    def _obs_space(self):        
+        state_space = spaces.Box(self.pos_lb, self.pos_ub, dtype = np.float32)
+        obs_dict = {"state": state_space}
+        
+        for cam_name, cam_config in self.camera_infos.items():
+            for modality_name in cam_config.keys():
+                key_name = f"{cam_name}-{modality_name}" 
+                
+                if modality_name == 'rgb':
+                    resolution = cam_config[modality_name]["resolution"]
+                    spec = spaces.Box(low=0, high=1, shape=resolution + (3,))
+                
+                elif modality_name == 'depth':
+                    max_depth = cam_config[modality_name]["max_depth"]
+                    resolution = cam_config[modality_name]["resolution"]
+                    spec = spaces.Box(low=0, high=max_depth, shape=resolution + (1,))
+                
+                elif modality_name == 'point_cloud':
+                    spec = spaces.Box(low=-np.inf, high=np.inf, shape=((cam_config[modality_name]["num_points"],) + (3,)))
+                    
+                else:
+                    raise RuntimeError("Modality not supported")              
+            
+            obs_dict[key_name] = spec
+            
+        if self.use_imagined:
     
-    def discretize_scan_observation(self,data,new_ranges):
-        """
-        Discards all the laser readings that are not multiple in index of new_ranges
-        value.
-        """
-        self._episode_done = False
         
-        discretized_ranges = []
-        mod = len(data.ranges)/new_ranges
-        
-        rospy.logdebug("data=" + str(data))
-        rospy.logdebug("new_ranges=" + str(new_ranges))
-        rospy.logdebug("mod=" + str(mod))
-        
-        for i, item in enumerate(data.ranges):
-            if (i%mod==0):
-                if item == float ('Inf') or numpy.isinf(item):
-                    discretized_ranges.append(self.max_laser_value)
-                elif numpy.isnan(item):
-                    discretized_ranges.append(self.min_laser_value)
-                else:
-                    discretized_ranges.append(int(item))
-                    
-                if (self.min_range > item > 0):
-                    rospy.logerr("done Validation >>> item=" + str(item)+"< "+str(self.min_range))
-                    self._episode_done = True
-                else:
-                    rospy.logdebug("NOT done Validation >>> item=" + str(item)+"< "+str(self.min_range))
-                    
-
-        return discretized_ranges
-        
-        
-    def get_vector_magnitude(self, vector):
-        """
-        It calculated the magnitude of the Vector3 given.
-        This is usefull for reading imu accelerations and knowing if there has been 
-        a crash
-        :return:
-        """
-        contact_force_np = numpy.array((vector.x, vector.y, vector.z))
-        force_magnitude = numpy.linalg.norm(contact_force_np)
-
-        return force_magnitude
