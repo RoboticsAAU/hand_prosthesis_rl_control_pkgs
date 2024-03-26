5d3f26a48e9f2a64b65df01ce548e3e5c1681047
ToRexZ <victor.risager@hotmail.com>
2024-03-26 09:28:41 +0000

    Fixed name.

--- move_hand/src/utils/ros_helper_functions.py
+++ move_hand/src/utils/ros_helper_functions.py
@@ -1,9 +1,9 @@
 import rospy
 from typing import Union
 
-def _is_connected(publisher: Union[rospy.Publisher, rospy.Subscriber]) -> bool:
+def _is_connected(pubsub: Union[rospy.Publisher, rospy.Subscriber]) -> bool:
     """ Check if the publisher or Publisher is connected to any other nodes. Returns True if any node is connected to the publisher."""
-    return publisher.get_num_connections() > 0
+    return pubsub.get_num_connections() > 0
 
 
 
