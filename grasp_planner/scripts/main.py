5ca55438b0f471d34b511ce722c0c5ad5e3ab7a3
DaniDoro <daniel.d@outlook.dk>
2024-04-01 20:02:41 +0000

    Small progress with graspit handler and restructuring

--- grasp_planner/scripts/main.py
+++ grasp_planner/scripts/main.py
@@ -1,14 +1,24 @@
-from grasp_planner.src.grasp_handler import GraspitHandler
-
+from grasp_planner.grasp_handler import GraspHandler
+from geometry_msgs.msg import Pose
 
 if __name__ == '__main__':
     
     config = {
-        "world_file": "mia_hand_all_world",
+        "robot_file": "mia_hand",
         "search_energy": "STRICT_AUTO_GRASP_ENERGY", # See graspit interface
         "num_obj_grasps": 20,
-        "obj_dir": "/objects",
+        "obj_dir": "/Development/graspit/models/objects/graspit_shapenet",
         "save_dir": "/grasps",
+        "num_steps": 70000,
     }
     
-    graspit_handler = GraspitHandler(config, debug = True)
+    graspit_handler = GraspHandler(config, debug = True)
+    
+    hand_pose = Pose()
+    hand_pose.position.x = 100.0
+    hand_pose.position.y = 100.0
+    hand_pose.position.z = 100.0
+    
+    graspit_handler.plan_all_grasps(hand_pose=hand_pose)
+    
+    
