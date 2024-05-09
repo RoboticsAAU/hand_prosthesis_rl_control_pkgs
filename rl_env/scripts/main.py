d89fff6fe7bb250e4558dc1b66320b87097f95c8
ToRexZ <victor.risager@hotmail.com>
2024-05-09 12:06:07 +0000

    Loading model to continue training. TB still does not work.

--- rl_env/scripts/main.py
+++ rl_env/scripts/main.py
@@ -29,6 +29,17 @@ with open(package_path + "/params/hand/mia_hand_params.yaml", 'r') as file:
 with open(rospack.get_path("sim_world") + "/config/joint_limits.yaml", 'r') as file:
     joint_limits = yaml.safe_load(file)
 
+
+# Continue Learning with the this model: If no model should be loaded, set model_to_load to None
+model_to_load = 'rl_model_09052024_8500_steps.zip'
+# model_to_load = None
+log_to_load = 'mia_hand_rl_PPO_09052024_1'
+
+
+checkpoint_location = package_path + "/logging/checkpoints"
+tensorboard_log = package_path + "/logging/tb_events/"
+tb_log_to_load = tensorboard_log + log_to_load
+
 # Current date as a string in the format "ddmmyyyy"
 algorithm_name = "PPO"
 env_name= "mia_hand_rl"
@@ -40,7 +51,7 @@ steps_per_episode = sim_config["move_hand"]["num_points"]/sim_config["move_hand"
 # Save a checkpoint every 1000 steps
 checkpoint_callback = CheckpointCallback(
     save_freq = 10*steps_per_episode,
-    save_path = package_path + "/logging/checkpoints",
+    save_path = checkpoint_location,
     name_prefix = "rl_model" + f"_{date_string}",
     save_replay_buffer = False,
     save_vecnormalize = False,
@@ -80,14 +91,22 @@ def main():
     
         
     # Instantiate the PPO model
-    model = PPO(
-        policy = "MultiInputPolicy",
-        env = rl_env,
-        verbose = 1, 
-        device = device,
-        policy_kwargs=get_3d_policy_kwargs(extractor_name="smallpn"), # Can either be "smallpn", "mediumpn" or "largepn". See sb3.common.torch_layers.py 
-        **rl_config["hyper_params"]
-    )
+    if model_to_load is not None:
+        model = PPO.load(
+            path=checkpoint_location + "/" + model_to_load,
+            tensorboard_log=tb_log_to_load
+            )
+        model.set_env(env=rl_env)
+    else:
+        model = PPO(
+            policy = "MultiInputPolicy",
+            tensorboard_log=tensorboard_log,
+            env = rl_env,
+            verbose = 1, 
+            device = device,
+            policy_kwargs=get_3d_policy_kwargs(extractor_name="smallpn"), # Can either be "smallpn", "mediumpn" or "largepn". See sb3.common.torch_layers.py 
+            **rl_config["hyper_params"]
+        )
     
     # Train the model
     timesteps = rl_config["general"]["num_episodes"]*steps_per_episode
